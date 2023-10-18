4be7a2b4c433b23a50e898f258707920f441a3a8
HaZ3R0 <megamackan@outlook.com>
2023-10-18 10:37:05 +0200

    Add project files.

---
+++ Uppgit4.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Uppgift_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            string taltext = Console.ReadLine();
+            int tal1 = int.Parse(taltext);
+            while (true)
+            {
+                {
+                    Console.WriteLine("Vill du skriva in ett tal till?, y, n");
+                    string svar = Console.ReadLine();
+                    if (svar == "n")
+                    {
+                        break;
+                    }
+                    Console.WriteLine("skriv in ett heltal till");
+                    string nytttaltext = Console.ReadLine();
+                    int nytttal = int.Parse(nytttaltext);
+                    if (nytttal > tal1)
+                    {
+                        tal1 = nytttal;
+                    }
+                }
+            }
+            Console.WriteLine("Det högsta talet du skrev var " + tal1);
+        }
+    }
+}
